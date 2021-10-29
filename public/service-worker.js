083b7d178cc640607b9ae47b4189a3c3a9d36945
Amanda Morgan <AmandaC0022@gmail.com>
2021-10-29 14:06:22 -0500

    updated server.js to include process.env

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,12 +3,12 @@ const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    "/public/css/style.css",
-    "/public/js/index.js",
-    "/manifest.webmanifest",
     "/favicon.ico",
-    "/public/icons/icon-192x192.png",
-    "/public/icons/icon-512x512.png",
+    "/assets/css/style.css",
+    "/assets/js/index.js",
+    "/manifest.webmanifest",
+    "/assets/icons/icon-192x192.png",
+    "/assets/icons/icon-512x512.png",
   ];
 
   // install
